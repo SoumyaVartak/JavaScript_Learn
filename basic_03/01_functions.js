38f8a9d2a45db555b6338c3cf1b6806921757204
SoumyaVartak <saumyavartak4@gmail.com>
2026-08-13 22:01:50 +0530

    added

--- basic_03/01_functions.js
+++ basic_03/01_functions.js
@@ -28,3 +28,36 @@ function loginUserMessage(username){
 
 }
 console.log(loginUserMessage("SOUMYA"))
+
+function calculateCartPrice(...num1){ // rest operator
+    return num1
+}
+console.log(calculateCartPrice(200,300,500))
+
+
+function calculateCartPrices(val1,val2,...num2){ // rest operator
+    return num2
+}
+console.log(calculateCartPrices(200,300,500,567))  // val1 has 200, val2 has 300 other value
+// are stored on num2
+
+const user = {
+    user:"soumya",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`user name is ${anyobject.user} and price is ${anyobject.price}`);
+}
+//    handleObject(user)
+// another way of handaling the object
+handleObject({
+    user:"sam",
+    price:1000
+})
+// lets pass aray
+const myneewArray = [200,400,600,100]
+function returnSecondValue(getarray){
+    return  getarray[1]
+}
+console.log(returnSecondValue(myneewArray))
